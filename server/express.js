f5e717c3b55e2bad1d42b64ce678ebc261e00749
Ernest Irabor <irabeny89@gmail.com>
2021-03-06 15:15:33 +0100

    update comment out devBundle for production

--- server/express.js
+++ server/express.js
@@ -27,7 +27,7 @@ const CURRENT_WORKING_DIR = process.cwd()
 const app = express()
 
 //comment out before building for production
-devBundle.compile(app)
+// devBundle.compile(app)
 
 // parse body params and attache them to req.body
 app.use(bodyParser.json())
