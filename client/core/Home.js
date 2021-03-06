1984e244841569bb654674c7ef1a6e2dbf1699a7
Ernest Irabor <irabeny89@gmail.com>
2021-03-06 17:43:28 +0100

    🔥 🎨 improve code structure and swap image file

--- client/core/Home.js
+++ client/core/Home.js
@@ -1,10 +1,10 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
 import CardMedia from '@material-ui/core/CardMedia'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
-import unicornbikeImg from './../assets/images/unicornbike.jpg'
+import passionBroughtUsHere from './../assets/images/passion-brought-us-here.jpg'
 import Grid from '@material-ui/core/Grid'
 import auth from './../auth/auth-helper'
 import FindPeople from './../user/FindPeople'
@@ -22,30 +22,21 @@ const useStyles = makeStyles(theme => ({
     marginBottom: theme.spacing(5)
   },
   title: {
-    padding:`${theme.spacing(3)}px ${theme.spacing(2.5)}px ${theme.spacing(2)}px`,
+    padding: `${theme.spacing(3)}px ${theme.spacing(2.5)}px ${theme.spacing(2)}px`,
     color: theme.palette.text.secondary
   },
   media: {
     minHeight: 400
-  },
-  credit: {
-    padding: 10,
-    textAlign: 'right',
-    backgroundColor: '#ededed',
-    borderBottom: '1px solid #d0d0d0',
-    '& a':{
-      color: '#3f4771'
-    } 
   }
 }))
 
-export default function Home({history}){
+export default function Home({ history }) {
   const classes = useStyles()
   const [defaultPage, setDefaultPage] = useState(false)
 
-  useEffect(()=> {
+  useEffect(() => {
     setDefaultPage(auth.isAuthenticated())
-    const unlisten = history.listen (() => {
+    const unlisten = history.listen(() => {
       setDefaultPage(auth.isAuthenticated())
     })
     return () => {
@@ -53,36 +44,35 @@ export default function Home({history}){
     }
   }, [])
 
-    return (
-      <div className={classes.root}>
-        { !defaultPage &&
-          <Grid container spacing={8}>
-            <Grid item xs={12}>
-              <Card className={classes.card}>
-                <Typography variant="h6" className={classes.title}>
-                  Home Page
+  return (
+    <div className={classes.root}>
+      { !defaultPage &&
+        <Grid container spacing={8}>
+          <Grid item xs={12}>
+            <Card className={classes.card}>
+              <Typography variant="h6" className={classes.title}>
+                Home Page
+              </Typography>
+              <CardMedia className={classes.media} image={passionBroughtUsHere} title="Unicorn Bicycle" />
+              <CardContent>
+                <Typography type="body1" component="p">
+                  Welcome to the Happen-social home page.
                 </Typography>
-                <CardMedia className={classes.media} image={unicornbikeImg} title="Unicorn Bicycle"/>
-                <Typography variant="body2" component="p" className={classes.credit} color="textSecondary">Photo by <a href="https://unsplash.com/@boudewijn_huysmans" target="_blank" rel="noopener noreferrer">Boudewijn Huysmans</a> on Unsplash</Typography>
-                <CardContent>
-                  <Typography type="body1" component="p">
-                    Welcome to the MERN Social home page. 
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
+              </CardContent>
+            </Card>
           </Grid>
-        }
-        {defaultPage &&
-          <Grid container spacing={8}>
-            <Grid item xs={8} sm={7}>
-              <Newsfeed/>
-            </Grid>
-            <Grid item xs={6} sm={5}>
-              <FindPeople/>
-            </Grid>
+        </Grid>
+      }
+      {defaultPage &&
+        <Grid container spacing={8}>
+          <Grid item xs={8} sm={7}>
+            <Newsfeed />
           </Grid>
-        }
-      </div>
-    )
+          <Grid item xs={6} sm={5}>
+            <FindPeople />
+          </Grid>
+        </Grid>
+      }
+    </div>
+  )
 }
