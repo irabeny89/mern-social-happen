947a844f72c91538c76264b2ba673ede31602845
Ernest Irabor <irabeny89@gmail.com>
2021-03-06 15:02:46 +0100

    dotenv integration

--- config/config.js
+++ config/config.js
@@ -4,6 +4,6 @@ dotenv.config()
 export default {
   env: process.env.NODE_ENV || "development",
   port: process.env.PORT || 3000,
-  jwtSecret: process.env.JWT_SECRET || "0d8afdccc5bacd293a8865f968b2d14a",
+  jwtSecret: process.env.JWT_SECRET || "my_jwt_secret",
   mongoUri: process.env.MONGODB_URI || process.env.MONGO_HOST || `mongodb://${process.env.IP || 'localhost'}:${process.env.MONGO_PORT || '27017'}/happen`
 }
