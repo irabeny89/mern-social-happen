9393ddda549d76dea441439a1a6909b703fa1037
Ernest Irabor <irabeny89@gmail.com>
2021-03-06 14:21:53 +0100

    dotenv configuration

--- config/config.js
+++ config/config.js
@@ -1,12 +1,9 @@
-const config = {
-  env: process.env.NODE_ENV || 'development',
-  port: process.env.PORT || 3000,
-  jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-  mongoUri: process.env.MONGODB_URI ||
-    process.env.MONGO_HOST ||
-    'mongodb://' + (process.env.IP || 'localhost') + ':' +
-    (process.env.MONGO_PORT || '27017') +
-    '/mernproject'
-}
+import dotenv from 'dotenv'
 
-export default config
+dotenv.config()
+export default {
+  env: process.env.NODE_ENV || "development",
+  port: process.env.PORT || 3000,
+  jwtSecret: process.env.JWT_SECRET || "0d8afdccc5bacd293a8865f968b2d14a",
+  mongoUri: process.env.MONGODB_URI || process.env.MONGO_HOST || `mongodb://${process.env.IP || 'localhost'}:${process.env.MONGO_PORT || '27017'}/happen`
+}
