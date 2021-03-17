d4f69061170c39d0dff0da68c63a9a770176d52c
Ernest Irabor <irabeny89@gmail.com>
2021-03-17 01:21:43 +0100

    comment out dotenv config before deployment

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
-import dotenv from 'dotenv'
+// import dotenv from 'dotenv'
 
-dotenv.config()
+// dotenv.config()
 export default {
   env: process.env.NODE_ENV || "development",
   port: process.env.PORT || 3000,
